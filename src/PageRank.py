afd3e7ec794da879f39091cb382e61355a198d35
PengHao <826113664@qq.com>
2017-05-20 11:09:08 +0800

    backup

--- src/PageRank.py
+++ src/PageRank.py
@@ -1,4 +1,4 @@
-import jieba
+import jieba, math
 import pickle, re, time, os
 from collections import namedtuple
 import numpy as np
@@ -33,46 +33,53 @@ class PageRank(object):
         file.close()
         return stopWords
 
-    def pageToSentences(self):
+    def pageToSentences(self, useCache=True):
         """
         把每个页面数据看成一个段落，并将这些页面句子拆分开来
         同时会去掉空格以及所有的标点符号
         :return:
         """
-        pattern1 = re.compile(r"。|？|！|!|\?")
-        pattern2 = re.compile(r"[\s+－：:\.\!\/_,$%^*()【】\[\]\"\']+|[+—！{}<>《》/\\，。？、~@#￥%…&*（）]+")
-        sentences = []
+        if useCache:
+            file = open(self.sentences_path)
+            sentences = pickle.load(file)
+            file.close()
+            return sentences
+        else:
+            pattern1 = re.compile(r"。|？|！|!|\?")
+            pattern2 = re.compile(r"[\s+－：:\.\!\/_,$%^*()【】\[\]\"\']+|[+—！{}<>《》/\\，。？、~@#￥%…&*（）]+")
+            sentences = []
 
-        for i in range(len(self.data)):
-            # 把最后的换行符去掉
-            page = self.data[i][:-1]
-            # 分成句子
-            print("{}、original data：{}".format(i, page))
-            strs = re.split(pattern1, page)
+            for i in range(len(self.data)):
+                # 把最后的换行符去掉
+                page = self.data[i][:-1]
+                # 分成句子
+                print("{}、original data：{}".format(i, page))
+                strs = re.split(pattern1, page)
 
-            for j in range(len(strs)):
-                str = strs[j]
-                # 去掉所有的标点符号
+                for j in range(len(strs)):
+                    str = strs[j]
+                    # 去掉所有的标点符号
 
-                # print("{}、original sentence：{}".format(i, str))
-                str = re.sub(pattern2, "", str)
-                # 排除过短的句子
-                if len(str)>12:
-                    print("{}、processed sentence:{}".format(i, str))
-                    sentence = Sentence(str, i)
-                    sentences.append(sentence)
+                    # print("{}、original sentence：{}".format(i, str))
+                    str = re.sub(pattern2, "", str)
+                    # 排除过短的句子
+                    if len(str) > 12:
+                        print("{}、processed sentence:{}".format(i, str))
+                        sentence = Sentence(str, i)
+                        sentences.append(sentence)
 
-        # 经过分析，发现合并句子比较合适。。。。
-        sentences = self.merge_sentences(sentences)
+            # 经过分析，发现合并句子比较合适。。。。
+            sentences = self.merge_sentences(sentences)
 
-        print("句子总数：{}".format(len(sentences)))
-        file = open(sentences_path, 'wb')
-        pickle._dump(sentences, file)
-        file.close()
+            print("句子总数：{}".format(len(sentences)))
 
-        del self.data
+            file = open(sentences_path, 'wb')
+            pickle._dump(sentences, file)
+            file.close()
 
-        return sentences
+            del self.data
+
+            return sentences
 
     def merge_sentences(self, sentences=None):
         if sentences is None:
@@ -108,78 +115,87 @@ class PageRank(object):
 
         return merged_sentences
 
-    def sentencesToVector(self, sentences=None):
+    def sentencesToVector(self, sentences=None, useCache=True, cachePath='../data/vectors.pkl'):
         """
         把句子转换成由词的tf—idf组成的向量，因为维数太大而且几乎都是稀疏向量
         所以决定降维到2000维左右
         结束后将处理得到的tf-idf矩阵存储到 ../data/vector.pkl
-        :return:
+        :return: 降维之后的tf_idf权重向量，每个句子的段落编号paragraph_list
         """
+        if useCache:
+            if os.path.exists(cachePath):
+                file = open(cachePath, 'rb')
+                weights, paragraph_list = pickle.load(file)
+                file.close()
+                return weights, paragraph_list
+        else:
+            if sentences is None:
+                sentences = self.loadSentences()
 
-        if sentences is None:
-            sentences = self.loadSentences()
+            # 句子段落编号
+            paragraph_list = []
 
-        # 句子段落编号
-        paragraph_list = []
+            words_list = []
+            for sentence in sentences:
+                seg_list = jieba.lcut(sentence.str, cut_all=False)
+                # 去停用词
+                seg_list = [word for word in seg_list if word not in self.stopWords]
+                words_list.append(" ".join(seg_list))
+                paragraph_list.append(sentence.paragraph)
 
-        words_list = []
-        for sentence in sentences:
-            seg_list = jieba.lcut(sentence.str, cut_all=False)
-            # 去停用词
-            seg_list = [word for word in seg_list if word not in self.stopWords]
-            words_list.append(" ".join(seg_list))
-            paragraph_list.append(sentence.paragraph)
+            # 开始转换为tf_idf矩阵
+            vectorizer = CountVectorizer()
+            transformer = TfidfTransformer()
+            print("开始计算tf—idf...")
+            start = time.time()
+            tf_idf_matrix = transformer.fit_transform(vectorizer.fit_transform(words_list))
+            end = time.time()
+            print("耗时：{}s".format(end - start))
 
-        # 开始转换为tf_idf矩阵
-        # vectorizer = CountVectorizer()
-        # transformer = TfidfTransformer()
-        # tf_idf_matrix = transformer.fit_transform(vectorizer.fit_transform(words_list))
-        #
-        # words = vectorizer.get_feature_names()
-        # tf_idf_matrix = tf_idf_matrix.toarray()
-        #
-        # print(len(words))
-        # print(tf_idf_matrix.shape)
-        # for i in tf_idf_matrix[0]:
-        #     if i!= 0:
-        #         print(i)
+            words = vectorizer.get_feature_names()
+            tf_idf_matrix = tf_idf_matrix.toarray()
 
-        ##########################################
-        #test，自己写的tf_idf
-        transformer = Tf_idfTransformer()
-        # 统计词频
-        tf_matrix, words = transformer.wordsToMatrix(words_list)
-        print("得到词频，开始计算tf—idf...")
-        start = time.time()
-        tf_idf_matrix = transformer.cal_tf_idf(tf_matrix, words, words_list)
-        end = time.time()
-        print("耗时：{}s".format(end-start))
+            print(len(words))
+            # print(tf_idf_matrix.shape)
+            # for i in tf_idf_matrix[0]:
+            #     if i!= 0:
+            #         print(i)
+            #
+            # ##########################################
+            # #test，自己写的tf_idf
+            # transformer = Tf_idfTransformer()
+            # # 统计词频
+            # tf_matrix, words = transformer.wordsToMatrix(words_list)
+            # print("得到词频，开始计算tf—idf...")
+            # start = time.time()
+            # tf_idf_matrix = transformer.cal_tf_idf(tf_matrix, words, words_list)
+            # end = time.time()
+            # print("耗时：{}s".format(end-start))
+            #
+            # print(tf_idf_matrix.shape)
 
-        print(tf_idf_matrix.shape)
+            print(words_list[0])
 
-        print(words_list[0])
+            for i in range(len(tf_idf_matrix[0])):
+                if tf_idf_matrix[0][i] != 0:
+                    print(words[i], tf_idf_matrix[0][i], sep=' ')
 
-        for i in range(len(tf_idf_matrix[0])):
-            if tf_idf_matrix[0][i] != 0:
-                print(words[i],tf_idf_matrix[0][i], sep=' ')
+            print('开始PCA降维。。。。')
+            start = time.time()
+            pca = PCA(n_components=2000)
+            weights = pca.fit_transform(tf_idf_matrix)
+            print(sum(pca.explained_variance_ratio_))
+            end = time.time()
+            print('结束，耗时{}s'.format(end - start))
 
-        print('开始PCA降维。。。。')
-        start = time.time()
-        pca = PCA(n_components= 2000)
-        weights = pca.fit_transform(tf_idf_matrix)
-        print(sum(pca.explained_variance_ratio_))
-        end = time.time()
-        print('结束，耗时{}s'.format(end-start))
-
-        print(weights.shape)
-        # for i in weights[0]:
-        #     if i != 0:
-        #         print(i)
-
-        file = open('../data/vectors.pkl', 'wb')
-        pickle.dump((weights,paragraph_list), file)
-        file.close()
-
+            print(weights.shape)
+            # for i in weights[0]:
+            #     if i != 0:
+            #         print(i)
+            file = open(cachePath, 'wb')
+            pickle.dump((weights, paragraph_list), file)
+            file.close()
+            return weights, paragraph_list
 
     def cutWords(self):
         """
@@ -206,7 +222,6 @@ class PageRank(object):
 
         print('去停用词后长度：{}'.format(len(seg_list)))
         # 总共有大概14404个不同的词语
-
 
 
     def loadSentences(self):
@@ -260,6 +275,47 @@ class PageRank(object):
         fig.subplots_adjust(hspace=0.4)
         plt.show()
 
+    def getNearness(self, vec1, vec2):
+        """
+        通过余弦夹角运算得到两个向量的相似度
+        :param vec1:
+        :param vec2:
+        :return: 相似度，越大表示越相近
+        """
+        assert len(vec1.shape) == 1 and len(vec1.shape) == 1
+        assert vec1.shape[0] == vec2.shape[0]
+
+        dot_mul = vec1.dot(vec2)
+        norm1 = np.sqrt(np.sum(np.square(vec1)))
+        norm2 = np.sqrt(np.sum(np.square(vec2)))
+
+        result = np.arccos(dot_mul/(norm1*norm2))
+
+        return (2*np.pi)/result
+
+
+    def page_rank(self, weights, paragraph_list, sentences, num=10, epsilon=0.0001, max_step=1e4, d=0.85):
+        sen_nums = weights.shape[0]
+
+        # page_rank的初始值
+        p = np.ones((sen_nums, ), dtype=np.float32)
+        p /= sen_nums
+        temp = np.zeros((sen_nums, ), dtype=np.float32)
+
+        # 构建图
+        graph = np.zeros((sen_nums, sen_nums), dtype=np.float32)
+
+        # 计算邻接图的权重
+        for i in range(sen_nums):
+            for j in range(sen_nums):
+                if j < i and paragraph_list[j] == paragraph_list[i]:
+                    graph[i][j] = self.getNearness(weights[i], weights[j])
+
+        for i in range(max_step):
+            pass
+        pass
+
+
 if __name__ == "__main__":
     dataPath = '../data/pages_content.pkl'
     sentences_path = '../data/sentences.pkl'
@@ -267,7 +323,7 @@ if __name__ == "__main__":
 
 
     pr = PageRank(dataPath=dataPath, sentences_path=sentences_path, stopWords_path=stopWords_path)
-    # sentences = pr.pageToSentences()
-    pr.sentencesToVector()
+    sentences = pr.pageToSentences(useCache=True)
+    weights, paragraph_list = pr.sentencesToVector(sentences=sentences, useCache=True)
     # pr.cutWords()
     # pr.analyze_sentences()
